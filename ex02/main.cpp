0379170aaa10df67d6ac17e2e613538887249cd2
Saliinger <alexisnoukan@gmail.com>
2024-12-21 00:55:24 +0100

    done ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Alexis Noukan on 20/12/2024.
-//
+#include <iostream>
+
+int main() {
+  std::string s = "HI THIS IS BRAIN";
+  std::string &stringREF = s;
+  std::string *stringPTR = &s;
+
+  std::cout << "This is string " <<s << std::endl;
+  std::cout << "This is string address " << &s << std::endl;
+  std::cout << "This is stringREF " << stringREF << std::endl;
+  std::cout << "This is stringREF address " << &stringREF << std::endl;
+  std::cout << "This is stringPTR " << *stringPTR << std::endl;
+  std::cout << "This is stringPTR address " << stringPTR << std::endl;
+  return 0;
+}
